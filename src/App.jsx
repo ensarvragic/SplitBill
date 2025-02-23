5454fd83c739e5f8cbc262689248ef69d4d87c42
Ensar <vragicensar@gmail.com>
2025-02-23 17:15:14 +0100

    initial changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,73 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
 
-function App() {
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Ensar",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSff7YZH0MdbRtsKTAPa9T7nDODRl2klaSUhg&s",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Amer",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSff7YZH0MdbRtsKTAPa9T7nDODRl2klaSUhg&s",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Ali",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSff7YZH0MdbRtsKTAPa9T7nDODRl2klaSUhg&s",
+    balance: 0,
+  },
+];
 
+export default function App() {
   return (
-    <>
-    <>In progress...</>
-    </>
-  )
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+      </div>
+    </div>
+  );
 }
 
-export default App
+function FriendList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <div>
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance === 0 && (
+        <p>
+          You and {friend.name} are even.
+        </p>
+      )}
+      </div>
+
+      <button className="button">Select</button>
+    </li>
+  );
+}
